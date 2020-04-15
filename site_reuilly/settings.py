bb1baeb1b09229b07e929a46fc31d4dd4db51e60
malaury <malaury.lemaitresalmon@gmail.com>
2020-04-15 12:09:37 +0200

    ok

--- site_reuilly/settings.py
+++ site_reuilly/settings.py
@@ -166,6 +166,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = None
 
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {
