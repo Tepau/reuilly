fede6bb352dd6eee6de5c97e443f56da8bead36e
malaury <malaury.lemaitresalmon@gmail.com>
2020-04-15 10:05:04 +0200

    ok

--- site_reuilly/settings.py
+++ site_reuilly/settings.py
@@ -168,13 +168,13 @@ if os.environ.get('ENV') == 'PRODUCTION':
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
+    }
+    AWS_LOCATION = 'static'
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_DIRS = [
+        os.path.join(PROJECT_ROOT, 'static'),
+    ]
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-DEFAULT_FILE_STORAGE = 'site_reuilly.storage_backends.MediaStorage'  # <-- here is where we reference it
+    DEFAULT_FILE_STORAGE = 'site_reuilly.storage_backends.MediaStorage'  # <-- here is where we reference it
