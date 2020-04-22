62a7982fda2620952f79717f4de004cabde2bdfd
malaury <malaury.lemaitresalmon@gmail.com>
2020-04-22 18:43:14 +0200

    admin

--- site_reuilly/settings.py
+++ site_reuilly/settings.py
@@ -178,23 +178,23 @@ DATABASES['default'].update(db_from_env)
 
 django_heroku.settings(locals())
 
-if os.environ.get('ENV') == 'PRODUCTION':
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = None
 
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_OBJECT_PARAMETERS = {
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = None
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
     }
-    AWS_LOCATION = 'static'
+AWS_LOCATION = 'static'
 
-    STATICFILES_DIRS = [
+STATICFILES_DIRS = [
         os.path.join(PROJECT_ROOT, 'static'),
     ]
-    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    DEFAULT_FILE_STORAGE = 'site_reuilly.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'site_reuilly.storage_backends.MediaStorage'
 
